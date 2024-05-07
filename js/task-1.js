3fff4cea887971cb1290ca9cf0520027e6078695
Serhii Tyron <mestn1y@hotmail.com>
2024-05-07 14:05:28 +0300

    hw3

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+function slugify(title) {
+  title = title.toLowerCase().split(" ");
+  return title.join("-");
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
